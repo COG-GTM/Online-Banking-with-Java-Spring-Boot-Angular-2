867a31f067733a7390ef458786e1487d421e0b01
patrick.bradley <patrick.bradley@cognition.ai>
2026-09-11 17:13:31 +0000

    bug: re-enable Spring Security CSRF protection (snyk java/DisablesCSRFProtection)

--- UserFront/src/main/java/com/userFront/config/SecurityConfig.java
+++ UserFront/src/main/java/com/userFront/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 permitAll().anyRequest().authenticated();
 
         http
-                .csrf().disable().cors().disable()
+                .cors().disable()
                 .formLogin().failureUrl("/index?error").defaultSuccessUrl("/userFront").loginPage("/index").permitAll()
                 .and()
                 .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/index?logout").deleteCookies("remember-me").permitAll()
